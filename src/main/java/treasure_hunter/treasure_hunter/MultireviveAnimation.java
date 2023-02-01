5bb30d06d5f3d6b881d250480626303e6dea0ef7
BuildTools <unconfigured@null.spigotmc.org>
2023-02-01 22:35:29 +0100

    Anfang Multirevive Animation

--- src/main/java/treasure_hunter/treasure_hunter/MultireviveAnimation.java
+++ src/main/java/treasure_hunter/treasure_hunter/MultireviveAnimation.java
@@ -1,16 +1,136 @@
 package treasure_hunter.treasure_hunter;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MultireviveAnimation {
     private Player player;
     private ArrayList<Entity> TextureList = new ArrayList<>();
+    private int State;
+    private int TotemCount = 0;
+    private GameData gameData;
+    private Treasure_Hunter main;
 
-    public MultireviveAnimation(GameData gameData, Player p) {
+    public MultireviveAnimation(Treasure_Hunter treasureHunter, GameData gameData, Player p) {
+        main = treasureHunter;
+        this.gameData = gameData;
         player = p;
+        State = 1;
+        createTextures();
+    }
+
+    public void createTextures() {
+        int i;
+        if (gameData.getRedPlayerList().contains(player.getName())) {
+            for (i = 0; i < gameData.getRedPlayerList().size(); i++) {
+                if (Bukkit.getPlayerExact(gameData.getRedPlayerList().get(i)) != null) {
+                    if (Objects.requireNonNull(Bukkit.getPlayer(gameData.getRedPlayerList().get(i))).getScoreboardTags().contains("dead")) {
+                        TotemCount++;
+                    }
+                }
+            }
+        }else {
+            for (i = 0; i < gameData.getBluePlayerList().size(); i++) {
+                if (Bukkit.getPlayerExact(gameData.getBluePlayerList().get(i)) != null) {
+                    if (Objects.requireNonNull(Bukkit.getPlayer(gameData.getBluePlayerList().get(i))).getScoreboardTags().contains("dead")) {
+                        TotemCount++;
+                    }
+                }
+            }
+        }
+        for (i = 0; i < TotemCount; i++) {
+            TextureList.add(player.getWorld().spawn(player.getLocation(), ArmorStand.class, entity -> {
+                Objects.requireNonNull(entity.getEquipment()).setHelmet(new ItemStack(Material.TOTEM_OF_UNDYING, 1));
+                entity.setGravity(false);
+                entity.setInvulnerable(true);
+                entity.setVisible(false);
+                entity.addScoreboardTag("MultireviveTexture");
+                entity.addEquipmentLock(EquipmentSlot.HAND, ArmorStand.LockType.ADDING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.HAND, ArmorStand.LockType.REMOVING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.OFF_HAND, ArmorStand.LockType.ADDING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.OFF_HAND, ArmorStand.LockType.REMOVING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.HEAD, ArmorStand.LockType.ADDING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.HEAD, ArmorStand.LockType.REMOVING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.CHEST, ArmorStand.LockType.ADDING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.CHEST, ArmorStand.LockType.REMOVING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.LEGS, ArmorStand.LockType.ADDING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.LEGS, ArmorStand.LockType.REMOVING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.FEET, ArmorStand.LockType.ADDING_OR_CHANGING);
+                entity.addEquipmentLock(EquipmentSlot.FEET, ArmorStand.LockType.REMOVING_OR_CHANGING);
+            }));
+        }
+    }
+
+    public void startAnimation() {
+        new BukkitRunnable() {
+            int number = 0;
+            @Override
+            public void run() {
+                if (State == 4) {
+                    reviveAnimation();
+                    cancel();
+                    return;
+                }
+                if (number <= 90) {
+                    number++;
+                }else {
+                    if (State == 1) {
+                        State = 2;
+                    }
+                    Animation();
+                    cancel();
+                }
+            }
+        }.runTaskTimer(main, 0L, 1L);
+    }
+
+    public void Animation() {
+        new BukkitRunnable() {
+            int number = 0;
+            @Override
+            public void run() {
+                number++;
+                if (State == 3) {
+                    endAnimation();
+                    cancel();
+                }
+                if (State == 4) {
+                    reviveAnimation();
+                    cancel();
+                }
+            }
+        }.runTaskTimer(main, 0L, 1L);
+    }
+
+    public void endAnimation() {
+        new BukkitRunnable() {
+            int number = 0;
+            @Override
+            public void run() {
+                number++;
+                if (State == 4) {
+                    reviveAnimation();
+                    cancel();
+                }
+                if (State == 1) {
+                    startAnimation();
+                    cancel();
+                }
+            }
+        }.runTaskTimer(main, 0L, 1L);
+    }
+
+    public void reviveAnimation() {
+
     }
 
     public Player getPlayer() {
